bc3951572f9f98dcddbf16c4dc0f452881c2afbd
Khyati Mahendru <khyati.ma25@gmail.com>
2020-09-23 22:36:18 +0530

    Added basic project structure

---
+++ TradeSurveillanceBackend/src/main/java/com/tradesurveil/demo/TradeSurveillanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tradesurveil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSurveillanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSurveillanceBackendApplication.class, args);
+	}
+
+}
---
+++ TradeSurveillanceBackend/src/test/java/com/tradesurveil/demo/TradeSurveillanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradesurveil.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSurveillanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
