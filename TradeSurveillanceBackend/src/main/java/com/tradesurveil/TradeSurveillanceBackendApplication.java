70375164e1d04b614e302c4f41fa6d19087383d5
krysellemartis <krysellesimaranmartis@gmail.com>
2020-09-29 13:00:11 +0530

    Fixes API calls

--- TradeSurveillanceBackend/src/main/java/com/tradesurveil/TradeSurveillanceBackendApplication.java
+++ TradeSurveillanceBackend/src/main/java/com/tradesurveil/TradeSurveillanceBackendApplication.java
@@ -2,8 +2,12 @@ package com.tradesurveil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import come.tradesurveil.controller.TradeController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = TradeController.class)
 public class TradeSurveillanceBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TradeSurveillanceBackendApplication.class, args);
