89911ee16571a3dd61197bfd0b8aa1f64ddac5fd
Khyati Mahendru <khyati.ma25@gmail.com>
2020-09-29 22:18:57 +0530

    Add Eureka server registery

--- TradeSurveillanceBackend/src/main/java/com/tradesurveil/TradeSurveillanceBackendApplication.java
+++ TradeSurveillanceBackend/src/main/java/com/tradesurveil/TradeSurveillanceBackendApplication.java
@@ -1,12 +1,19 @@
 package com.tradesurveil;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.tradesurveil.controller.TradeController;
 
+@EnableAutoConfiguration
+@Configuration
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackageClasses = TradeController.class)
 public class TradeSurveillanceBackendApplication {
 	public static void main(String[] args) {
