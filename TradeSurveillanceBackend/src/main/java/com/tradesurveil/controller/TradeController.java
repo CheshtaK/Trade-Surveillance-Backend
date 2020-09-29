909b0b486515d651be7a351dfaf18ae7c9bd449e
krysellemartis <krysellesimaranmartis@gmail.com>
2020-09-29 17:21:54 +0530

    Adds cross origin

--- TradeSurveillanceBackend/src/main/java/com/tradesurveil/controller/TradeController.java
+++ TradeSurveillanceBackend/src/main/java/com/tradesurveil/controller/TradeController.java
@@ -1,11 +1,10 @@
 package com.tradesurveil.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +17,7 @@ import com.tradesurveil.bean.TradeForDataGen;
 import com.tradesurveil.businesslogic.DetectFrontRunning;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TradeController {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		TradeJDBCTemplate tradeJDBCTemplate = (TradeJDBCTemplate)context.getBean("tradeJDBCTemplate");
