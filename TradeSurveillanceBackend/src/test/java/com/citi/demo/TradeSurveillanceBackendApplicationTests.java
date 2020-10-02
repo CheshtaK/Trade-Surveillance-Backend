e4f947e8e031bc0a9294fb6429efc02b01daf473
Khyati Mahendru <khyati.ma25@gmail.com>
2020-10-02 17:48:49 +0530

    Revert "Add jar and war"
    This reverts commit 60c6b1dfb29cd63949714877aff820eafd14a2d2.

---
+++ TradeSurveillanceBackend/src/test/java/com/citi/demo/TradeSurveillanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSurveillanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
